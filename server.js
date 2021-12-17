6fa9c24511411bbc71b3f4d12ddd0a236440d9cf
jamiguel23 <josematthewmiguel@gmail.com>
2021-12-17 15:06:07 -0800

    inital commit

---
+++ server.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/test', (request, response) => {
+  
+  response.send('test request received')
+  
+})
+
+
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
