d62a794ae7b859d63b53ca7aff25f8fc9b665587
MikeKuhl <38599344+MikeKuhl@users.noreply.github.com>
2021-12-20 15:48:16 -0800

    fixed covid test

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 "use strict";
 
+const axios = require("axios");
 const express = require("express");
 const cors = require("cors");
+const covidData = require("./covidData");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
@@ -9,9 +11,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-
-
+app.get("/covidData", covidData);
 app.get("/test", (request, response) => {
   response.send("test request received");
 });
