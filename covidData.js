f73160f9b362527eee883f3737df8030b8202024
MikeKuhl <38599344+MikeKuhl@users.noreply.github.com>
2021-12-27 09:16:28 -0800

    oauth setup

--- covidData.js
+++ covidData.js
@@ -3,27 +3,17 @@
 const axios = require("axios");
 
 async function handleGetData(req, res) {
-
   try {
     const url = `https://localcoviddata.com/covid19/v1/cases/newYorkTimes?zipCode=98118&daysInPast=7`;
     const resultData = await axios.get(url);
 
     const responseData = resultData.data.counties[0].historicData;
-    console.log(responseData)
+    console.log(responseData);
     //returns "date", "deathCt", "positiveCt"
     res.status(200).send(responseData);
-
   } catch (e) {
-    res.status(500).send('Server Error')
+    res.status(500).send("Server Error");
   }
 }
 
-
-
-
-
-
-
 module.exports = handleGetData;
-
-
